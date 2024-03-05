f165236d88a38a8bc7f701570ec4abdbc48006c4
Bjørn Otto Hansen <bjorn-otto@zebraconsulting.no>
2024-03-05 15:34:37 +0100

    Removed test commend in infrastructure project, should trigger build

--- src/PulumiDemo.Infrastructure/Program.cs
+++ src/PulumiDemo.Infrastructure/Program.cs
@@ -5,6 +5,4 @@ namespace PulumiDemo.PersonService.Infrastructure;
 class Program
 {
     static async Task<int> Main() => await Deployment.RunAsync<AzureStack>();
-
-    // Test-comment in infrastructure, should trigger a deploy
 }
