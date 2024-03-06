2f2200e549a7e54a1a8a7ecb70746ad674090332
Bjørn Otto Hansen <bjorn-otto@zebraconsulting.no>
2024-03-06 15:44:47 +0100

    Minor fixes

--- infrastructure/Names.cs
+++ infrastructure/Names.cs
@@ -2,11 +2,11 @@ namespace PulumiDemo.PersonService.Infrastructure;
 
 public record Names(string ProjectName, string ProjectShortName, string Environment)
 {
-    public string ResourceGroup => $"{SanitizeResourceName(ProjectName)}{Environment}-rg";
+    public string ResourceGroup => $"{SanitizeResourceName(ProjectName)}-{Environment}-rg";
     public string StorageAccount => $"{SanitizeResourceName(ProjectShortName)}{Environment}sa";
 
     private static string SanitizeResourceName(string name) => name
-        .ToLower()
+        .ToLowerInvariant()
         .Replace(".", "-")
         .Trim();
 }
